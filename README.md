882a3bc769bf9ae0f1a2781943ce58dcf0a07505
Y.Matsuda <ymatsuda.zck@gmail.com>
2024-03-24 23:04:18 +0900

    add generated otelcol codes and docs

---
+++ README.md
@@ -0,0 +1,43 @@
+# otel-tui
+
+🚧 This project is under construction 🚧
+
+A terminal OpenTelemetry viewer inspired by [otel-desktop-viewer](https://github.com/CtrlSpice/otel-desktop-viewer/tree/main)
+
+![Traces](./docs/traces.png)
+![Spans](./docs/spans.png)
+
+
+## Getting Started
+
+```sh
+# install the CLI tool
+go install github.com/ymtdzzz/otel-tui@main
+
+# run the CLI tool (running on localhost:4317 by default)
+otel-tui
+```
+
+## TODOs
+
+There're a lot of things to do. Here are some of them:
+
+- Traces
+  - [x] Display traces
+  - [ ] Filter traces
+  - [ ] Show trace information
+  - [ ] ...
+- Metrics
+  - [ ] Display metrics
+  - [ ] ...
+- Logs
+  - [ ] Display logs
+  - [ ] ...
+- UI
+  - [ ] Improve UI
+  - [ ] Add more keybindings
+  - [ ] ...
+- Performance
+  - [ ] Timer based refresh
+  - [ ] Data rotation
+  - [ ] ...
---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"log"
+	"strconv"
+
+	"github.com/spf13/cobra"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/confmap"
+	"go.opentelemetry.io/collector/confmap/provider/yamlprovider"
+	"go.opentelemetry.io/collector/otelcol"
+)
+
+func main() {
+	info := component.BuildInfo{
+		Command:     "otelcol",
+		Description: "OpenTelemetry Collector with TUI viewer",
+		Version:     "1.0.0",
+	}
+
+	if err := run(otelcol.CollectorSettings{BuildInfo: info, Factories: components}); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func runInteractive(params otelcol.CollectorSettings) error {
+	//cmd := otelcol.NewCommand(params)
+	cmd := newCommand(params)
+	if err := cmd.Execute(); err != nil {
+		log.Fatalf("collector server run finished with error: %v", err)
+	}
+
+	return nil
+}
+
+func newCommand(params otelcol.CollectorSettings) *cobra.Command {
+	var httpPortFlag, grpcPortFlag int
+	var hostFlag string
+
+	rootCmd := &cobra.Command{
+		Use:          params.BuildInfo.Command,
+		Version:      params.BuildInfo.Version,
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			configContents := `yaml:
+receivers:
+  otlp:
+    protocols:
+      http:
+        endpoint: ` + hostFlag + `:` + strconv.Itoa(httpPortFlag) + `
+      grpc:
+        endpoint: ` + hostFlag + `:` + strconv.Itoa(grpcPortFlag) + `
+
+processors:
+
+exporters:
+  tui:
+
+service:
+  pipelines:
+    traces:
+      receivers: [otlp]
+      processors: []
+      exporters: [tui]
+`
+
+			provider := yamlprovider.New()
+			configProviderSettings := otelcol.ConfigProviderSettings{
+				ResolverSettings: confmap.ResolverSettings{
+					URIs:      []string{configContents},
+					Providers: map[string]confmap.Provider{provider.Scheme(): provider},
+				},
+			}
+
+			params.ConfigProviderSettings = configProviderSettings
+
+			col, err := otelcol.NewCollector(params)
+			if err != nil {
+				return err
+			}
+			return col.Run(cmd.Context())
+		},
+	}
+
+	rootCmd.Flags().IntVar(&httpPortFlag, "http", 4318, "The port number on which we listen for OTLP http payloads")
+	rootCmd.Flags().IntVar(&grpcPortFlag, "grpc", 4317, "The port number on which we listen for OTLP grpc payloads")
+	rootCmd.Flags().StringVar(&hostFlag, "host", "localhost", "The host where we expose our all endpoints (OTLP receivers and browser)")
+	return rootCmd
+}
