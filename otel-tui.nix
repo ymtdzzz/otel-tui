4e42c7f4250598c44ecb65b919ac7e2522218d72
ymtdzzz <44557218+ymtdzzz@users.noreply.github.com>
2025-04-12 09:21:44 +0000

    [Automated] Update Nix Flake

--- otel-tui.nix
+++ otel-tui.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  otel-tui-version = "v0.4.8";
+  otel-tui-version = "v0.4.9";
 in
 pkgs.buildGoModule {
   pname = "otel-tui";
@@ -15,7 +15,7 @@ pkgs.buildGoModule {
     owner = "ymtdzzz";
     repo = "otel-tui";
     rev = "${otel-tui-version}";
-    hash = "sha256-cy3gHECl1V26TWGQuzkWg2iKIBqpQsnAMgH021uindE=";
+    hash = "sha256-mIvorgBikhO3pztTYMJqbdzEnWUS9MUgW0/Mr57b4Pg=";
   };
   overrideModAttrs = (
     _: {
@@ -27,7 +27,7 @@ pkgs.buildGoModule {
   ldflags = [
     "-X main.version=${otel-tui-version}"
   ];
-  vendorHash = "sha256-tjzWpiNueFmKBZriYac/q7zACJ8GeWOHy4wnr+Lnafc=";
+  vendorHash = "sha256-GoZnjApgLPMq/23jbbFf1ooGBTa36yRJZjRMh1Ufx9I=";
   subPackages = [ "." ];
   buildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux [
     pkgs.xorg.libX11
