87d749632429bfc7c7951391913d5c3b953ac2ed
Adrian Cole <adrian@tetrate.io>
2025-12-09 14:40:24 +0100

    fix: prevent panic when metric chart has more keys than colors
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- tuiexporter/internal/tui/component/page/metric/chart_test.go
+++ tuiexporter/internal/tui/component/page/metric/chart_test.go
@@ -1,13 +1,17 @@
 package metric
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/ymtdzzz/otel-tui/tuiexporter/internal/telemetry"
 	"github.com/ymtdzzz/otel-tui/tuiexporter/internal/test"
 	"github.com/ymtdzzz/otel-tui/tuiexporter/internal/tui/component/layout"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/pmetric"
 )
 
 func TestDrawMetricHistogramChart(t *testing.T) {
@@ -83,4 +87,53 @@ func TestChartInputCaptureAfterFlush(t *testing.T) {
 
 	got := gotInputCapture(tcell.NewEventKey(tcell.KeyRight, ' ', tcell.ModNone))
 	assert.Nil(t, got)
+}
+
+func TestGetDataToDrawWithMoreKeysThanColors(t *testing.T) {
+	// Create dataMap with 12 keys (more than 10 colors available)
+	// This test ensures no panic occurs when there are more keys than colors
+	attrkey := "test_attr"
+	numKeys := 12
+
+	dataMap := make(map[string]map[string][]*pmetric.NumberDataPoint)
+	dataMap[attrkey] = make(map[string][]*pmetric.NumberDataPoint)
+
+	start := time.Now()
+	end := start.Add(time.Minute)
+
+	for i := 0; i < numKeys; i++ {
+		key := fmt.Sprintf("key_%02d", i)
+		// Use multiple data points per key to exercise interpolation logic
+		dps := make([]*pmetric.NumberDataPoint, 0, 3)
+		for j := 0; j < 3; j++ {
+			dp := pmetric.NewNumberDataPoint()
+			// Alternate between double and int values
+			if (i+j)%2 == 0 {
+				dp.SetDoubleValue(float64(i*10 + j))
+			} else {
+				dp.SetIntValue(int64(i*10 + j))
+			}
+			dp.SetTimestamp(pcommon.NewTimestampFromTime(start.Add(time.Duration(i*3+j) * time.Second)))
+			dps = append(dps, &dp)
+		}
+		dataMap[attrkey][key] = dps
+	}
+
+	chart := newChart(layout.NewCommandList(), nil, []*layout.ResizeManager{})
+
+	// This should not panic
+	data, tv := chart.getDataToDraw(dataMap, attrkey, start, end)
+
+	// Verify we got data for all 12 keys
+	assert.Len(t, data, numKeys)
+
+	// Verify the legend text contains all keys with colors (colors wrap around)
+	legendText := tv.GetText(false)
+	for i := 0; i < numKeys; i++ {
+		key := fmt.Sprintf("key_%02d", i)
+		assert.Contains(t, legendText, key)
+		// Verify color tag is present (colors wrap via modulo)
+		expectedColor := layout.Colors[i%len(layout.Colors)].String()
+		assert.Contains(t, legendText, expectedColor)
+	}
 }
